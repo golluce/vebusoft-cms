93d49c400483f5c0dbcf0f8f2d1bb61a9f97e11a
Mücahit Bayram <golluce@gmail.com>
2025-04-25 00:30:31 +0300

    ✅ Admin yerine panel olarak yapı güncellendi

---
+++ resources/views/layouts/panel.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.panel')
+
+@section('content')
+    <h1 class="text-2xl font-bold">🎉 Vebusoft Panel'e Hoş Geldin Mücahit!</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+use App\Http\Controllers\Panel\DashboardController;
+
+Route::prefix('panel')->middleware(['auth'])->group(function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('panel.dashboard');
 });
